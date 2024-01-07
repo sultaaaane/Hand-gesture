c935a776ed2d18c6d6a2fef8952e3fc27a1eb5ad
sultaaaane <mohammed_bentahiralaoui@um5.ac.ma>
2024-01-07 17:17:27 +0100

    d

--- train_classifier.py
+++ train_classifier.py
@@ -1,28 +1,28 @@
 import pickle
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-import matplotlib.pyplot as plt
 
 data_dict = pickle.load(open('./data.pickle', 'rb'))
 
-data_dict['data'] = np.array(data_dict['data'])
-data_dict['labels'] = np.array(data_dict['labels'])
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
 
-x_train, x_test, y_train, y_test = train_test_split(data_dict['data'], data_dict['labels'], test_size=0.2,shuffle=True,stratify=data_dict['labels'])
+x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 model = RandomForestClassifier()
 
 model.fit(x_train, y_train)
 
-y_pred = model.predict(x_test)
+y_predict = model.predict(x_test)
 
-score = accuracy_score(y_test, y_pred)
+score = accuracy_score(y_predict, y_test)
 
-print('Accuracy: {}'.format(score))
+print('{}% of samples were classified correctly !'.format(score * 100))
 
-f = open('model.pickle', 'wb')
-pickle.dump(model, f)
+f = open('model.p', 'wb')
+pickle.dump({'model': model}, f)
 f.close()
